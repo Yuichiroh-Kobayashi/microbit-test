abe60a38b1b391a48ef4a9214253878a4f657ece
Yuichiroh-Kobayashi <qcho0613@gmail.com>
2023-06-02 22:05:52 +0900

    足回り動作を追加

--- main.ts
+++ main.ts
@@ -1,11 +1,31 @@
-motor.servo(motor.Servos.S1, 0)
-motor.stepperTurnDual_42(
-motor.Stepper.Ste1,
-motor.Dir.CW,
-0,
-motor.Dir.CW,
-0
-)
+function 左回転 () {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 128)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 128)
+}
+function 後退 () {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 128)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 128)
+}
+function 前進 () {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 128)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 128)
+}
+function 右回転 () {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 128)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 128)
+}
+motor.motorStopAll()
 basic.forever(function () {
-	
+    basic.showIcon(IconNames.Happy)
+    前進()
+    basic.pause(1000)
+    右回転()
+    basic.pause(2000)
+    左回転()
+    basic.pause(2000)
+    後退()
+    basic.pause(1000)
+    motor.motorStopAll()
+    basic.showIcon(IconNames.No)
+    basic.pause(5000)
 })
